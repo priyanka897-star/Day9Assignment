bb40fb8be120a41f92b782c462da0413c05d052f
priyanka897-star <83417119+priyanka897-star@users.noreply.github.com>
2021-11-26 21:54:42 +0530

    Refactored :Indentation Correction

--- EmployeeAttendanceUC1.sh
+++ EmployeeAttendanceUC1.sh
@@ -2,13 +2,9 @@ isPresent=1
 randomCheck=$(( RANDOM%2 ));
 if [ $isPresent == $randomCheck ]
 then
-     
-    echo "Employee is Present"
-    
+    echo "Employee is Present 
 else
      echo "Employee is Absent"
-
-
 fi
 
 
